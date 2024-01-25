20c60c03d58c390d2e5de64fbb68e6b2ecf2ca3a
JoKing1204 <jking24@basecampcodingacademy.org>
2024-01-25 08:47:35 -0600

    setups

--- app/models.py
+++ app/models.py
@@ -6,4 +6,4 @@ class Customer(models.Model):
     first_name = models.TextField()
     last_name = models.TextField()
     number = models.IntegerField()
-    email = models.EmailField()
+    email = models.EmailField() 
